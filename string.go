fa6605a4df76b67297471414c88bb44b737f9822
tujiaw <tujiaw@163.com>
2019-09-04 14:21:40 +0800

    add

--- string.go
+++ string.go
@@ -36,3 +36,29 @@ func Uuidv4() (string, error) {
 	}
 	return fmt.Sprintf("%X-%X-%X-%X-%X", b[0:4], b[4:6], b[6:8], b[8:10], b[10:]), nil
 }
+
+func FormatBytes(size float64) string {
+	const KB = float64(1024)
+	const MB = float64(KB * KB)
+	const GB = float64(MB * MB)
+
+	trimZero := func(str string) string {
+		for i := len(str) - 1; i >= 0; i-- {
+			if str[i] != 48 && str[i] != 46 {
+				return str[:i+1]
+			}
+		}
+		return str
+	}
+
+	switch {
+	case size < KB:
+		return "1 KB"
+	case size >= KB && size < MB:
+		return trimZero(fmt.Sprintf("%.2f", size/KB)) + " KB"
+	case size >= MB && size < GB:
+		return trimZero(fmt.Sprintf("%.2f", size/MB)) + " MB"
+	default:
+		return trimZero(fmt.Sprintf("%.2f", size/GB)) + " GB"
+	}
+}
